cef153fb91a47b6f7a12f37efdece11710fd5b7f
myssyb <beulahagiande@gmail.com>
2024-01-14 04:44:03 -0800

    indentation wahala

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,14 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    lastdigit = number % -10
+    last_digit = number % -10
 else:
-    lastdigit = number % 10
-if lastdigit > 5:
-    print("Last digit of {:d} is {:d} and is greater that 5".format(number, lastdigit))
-elif lastdigit < 6 and lastdigit != 0:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, lastdigit))
+    last_digit = number % 10
+if last_digit > 5:
+    print("Last digit of {:d} is {:d} and is greater that 5".format
+         (number, last_digit))
+elif last_digit < 6 and last_digit != 0:
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format
+         (number, last_digit))
 else:
-    print("Last digit of {:d} is 0 and is 0".format(numner))
+    print("Last digit of {:d} is 0 and is 0".format(number))
